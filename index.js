b28726a4927691158b10c6604cb8d217c5ed5843
hyunho9304 <32284944+hyunho9304@users.noreply.github.com>
2018-11-07 18:41:48 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-//	schedule 관련
-const schedule = require( './schedule' ) ;
-router.use( '/schedule' , schedule ) ;
-
-module.exports = router;
