51879332ac3aafc56d199a2f66c3d22bdbb54aa7
Unknown <gusgh1492@mail.hongik.ac.kr>
2019-05-21 20:25:04 +0900

    COMMIT // weather/list folder

--- SurfAdvisor_API/routes/index.js
+++ SurfAdvisor_API/routes/index.js
@@ -13,4 +13,17 @@ router.use( '/surfArea' , surfArea ) ;
 const setting = require( './setting/index' ) ;
 router.use( '/setting' , setting ) ;
 
+
+//	weather 관련
+const weather = require( './weather/index' ) ;
+router.use( '/weather' , weather ) ;
+
+
+
+
+//	도메인 설정 위한 통신상태 확인 인증
+router.get( '/' , function( req , res ) {
+	res.status(200).send("Welcome advisor surf!" ) ;
+})
+
 module.exports = router;
